b483623e40a067aa0018913156207187bde546ae
jhahn678 <jhahn678@yahoo.com>
2022-11-13 21:29:34 -0500

    Login hook

---
+++ hooks/mutations/useLogin.ts
@@ -0,0 +1,43 @@
+import axios from "../../config/axios";
+import { AuthResponse } from '../../types/User'
+import { useState } from 'react'
+
+interface UseLoginArgs {
+    onSuccess: (data: AuthResponse) => void,
+    onError: (err: any) => void
+}
+
+interface LoginParams {
+    identifier: string,
+    password: string
+}
+
+export const useLogin = ({ onSuccess, onError}: UseLoginArgs) => {
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
+
+    const loginUser = async (params: LoginParams): Promise<AuthResponse | void> => {
+        setIsLoading(true)
+        axios.post<AuthResponse>('/auth/login', params)
+            .then(({ data }) => {
+                setIsLoading(false)
+                onSuccess(data)
+                return data;
+            })
+            .catch(err => {
+                console.error(err)
+                setIsLoading(false)
+                setIsError(true)
+                onError(err)
+            })
+        
+    }
+
+    return {
+        loginUser,
+        isLoading,
+        isError
+    }
+
+}
