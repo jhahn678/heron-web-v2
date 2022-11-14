41acb8e551417764abe8017398b3b1748f389ba6
jhahn678 <jhahn678@yahoo.com>
2022-11-14 11:41:38 -0500

    hook with password validation logic

--- hooks/utils/useValidatePassword.ts
+++ hooks/utils/useValidatePassword.ts
@@ -1,14 +1,12 @@
-import * as yup from 'yup'
 import { useState, useEffect } from 'react'
 
 export const useValidatePassword = (value: string): Boolean => {
 
     const [isValid, setIsValid] = useState(false)
-    const schema = yup.string().matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
 
     useEffect(() => {
         const timer = setTimeout(async () => {
-            const result = await schema.isValid(value)
+            const result = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm.test(value)
             setIsValid(result)
         }, 300)
 
