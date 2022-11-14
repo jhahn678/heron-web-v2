d2f95f949512a485c3f10573dbd9707f44505c46
jhahn678 <jhahn678@yahoo.com>
2022-11-14 11:40:35 -0500

    Implementing Geolocated API

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import { ThemeProvider } from '@mui/material'
+import Modal from '../components/modal/Modal'
 import theme from '../config/theme'
+import { useCurrentLocation } from '../hooks/utils/useCurrentLocation'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+  useCurrentLocation()
+
   return( 
     <ThemeProvider theme={theme}>
         <Component {...pageProps} />
+        <Modal/>
     </ThemeProvider>
   )
 }
