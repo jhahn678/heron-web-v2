3e8c0b4da8b4996b2ba845068c382c17f3b136f3
jhahn678 <jhahn678@yahoo.com>
2022-11-22 15:48:59 -0500

    Default headers for axios

--- config/axios.ts
+++ config/axios.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
+import { Tokens } from '../types/LocalStorage';
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(Tokens.accessToken)}`
 
 export default axios
