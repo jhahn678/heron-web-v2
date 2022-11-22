814a6e6fa3770f63a02b3cb3e2465775cfee93f3
jhahn678 <jhahn678@yahoo.com>
2022-11-22 18:30:24 -0500

    Added utility function to axios config to delay local storage until available

--- config/axios.ts
+++ config/axios.ts
@@ -1,8 +1,12 @@
-import axios from 'axios'
+import Axios from 'axios'
 import { Tokens } from '../types/LocalStorage';
+import { getFromLocalStorage } from '../utils/localStorage/getFromLocalStorage';
 
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const axios = Axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    headers: {
+        Authorization: `Bearer ${getFromLocalStorage(Tokens.accessToken)}`
+    }
+});
 
-axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(Tokens.accessToken)}`
-
-export default axios
+export default axios;
