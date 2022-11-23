7696eaf84143d5d5efd9e7bfa9f53cc1894e5687
jhahn678 <jhahn678@yahoo.com>
2022-11-22 19:45:48 -0500

    Fixed bug where autoSignIn was being called twice and failing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,18 +6,24 @@ import { apolloClient } from '../config/apollo'
 import theme from '../config/theme'
 import { useCurrentLocation } from '../hooks/utils/useCurrentLocation'
 import { SnackbarProvider } from 'notistack';
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useAuth } from '../hooks/store/useAuth'
 import { Tokens } from '../types/LocalStorage'
 
 function MyApp({ Component, pageProps }) {
 
-  const { autoSignIn } = useAuth()
-
   useCurrentLocation()
 
+  const autoSignIn = useAuth(store => store.autoSignIn)
+
+  const signInAttempted = useRef(false)
+
   useEffect(() => {
-    if(localStorage.getItem(Tokens.accessToken)) autoSignIn()
+    if(signInAttempted.current) return;
+    if(localStorage.getItem(Tokens.accessToken)){
+      signInAttempted.current = true;
+      autoSignIn()
+    }
   },[])
 
   return( 
