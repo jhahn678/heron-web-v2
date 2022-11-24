beffc1c008e89bd9195ff1773e325f41a0f35e6c
jhahn678 <jhahn678@yahoo.com>
2022-11-24 10:08:53 -0500

    Add enum for auth error messages related to tokens

---
+++ types/Auth.ts
@@ -0,0 +1,7 @@
+export enum AuthErrors {
+    accessTokenInvalid = 'Access token invalid',
+    accessTokenExpired = 'Access token expired',
+    accessTokenRequired = 'Access token not provided in request body',
+    refreshTokenInvalid = 'Refresh token invalid',
+    refreshTokenExpired = 'Refresh token expired', 
+}
